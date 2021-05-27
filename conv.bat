7f01dbb0283c910c2e085affb91910153df888af
rxia24 <rxia24@yahoo.com>
2021-05-27 22:36:02 +0800

    Add files via upload

---
+++ conv.bat
@@ -0,0 +1,20 @@
+@echo off
+if not exist *.txt goto a1
+ren *.txt *.nota
+:a1
+
+
+yagai_con.exe *.mpx
+
+mkdir TEXT
+move *.txt TEXT\ >dummy
+del dummy
+
+ifÅ@not exist *.nota goto a2
+ren *.nota *.txt
+:a2
+
+echo Finish
+echo ÅiPRESS ENTERÅj
+pause
+exit
